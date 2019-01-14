c6efacf48b884a47ae5059731c51920b48f1f6eb
Tion W <tion@Tions-Mac.local>
2019-01-14 14:56:21 -0800

    Initial Commit

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by Tion W on 1/14/19.
+//  Copyright © 2019 Tion Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
